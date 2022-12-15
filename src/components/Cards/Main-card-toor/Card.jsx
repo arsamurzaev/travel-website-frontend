6632bb396efd8d72a0ec639eee98d5b3c885a7f6
bullet789 <ddzhukaev9536@gmail.com>
2022-12-15 16:59:23 +0300

    c

---
+++ src/components/Cards/Main-card-toor/Card.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./CardMain.module.scss";
+import img from "../../../Images/Rectangle9.png";
+import { Carousel } from "react-bootstrap";
+
+export default function Card () {
+  return (
+    <>
+      <div className={styles.card}>
+        <Carousel>
+          <Carousel.Item>
+            <img className="d-block w-100" src={img} alt="none" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={img} alt="none" />
+            
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={img} alt="none" />
+            
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={img} alt="none" />
+            
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={img} alt="none" />
+            
+          </Carousel.Item>
+        </Carousel>
+            <button className={styles.heart}>♡</button>
+
+        <div className={styles.dosc}>
+          <div className={styles.info}>
+            <div className={styles.city}>Абхазия, Пицунда</div>
+            <div className={styles.nameHome}>"Эдем", гостевой дом</div>
+            <div className={styles.nomer}>
+              Bed & Breakfast | Двухместный номер
+            </div>
+          </div>
+
+          <button className={styles.btn}>от 61 028 ₽ </button>
+        </div>
+      </div>
+    </>
+  );
+}
