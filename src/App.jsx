c6c33a391c65b5ad35ec94f11c0d33b23bd06d07
Deni <arsamurzaev_di@mail.ru>
2022-12-16 17:28:59 +0300

    Делаю главную страницу

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import Header from "./components/Header";
+import FilterMenu from "./components/Main/FilterMenu";
+import Navbar from "./components/Main/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MainPage from "./pages/MainPage";
 
 
 
 const App = () => {
   return (
     <div className="App">
-      
+      <Header/>
+      <MainPage/>
     </div>
   );
 }
