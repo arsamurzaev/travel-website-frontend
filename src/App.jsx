0f6068395b818c94b2504a555fba56029c791719
Alialisk <99403653+Aliasik@users.noreply.github.com>
2022-12-21 14:07:33 +0300

    ничего не изменил
    Э

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PersonalData from './components/personalData/personalData';
 
 
 const App = () => {
   return (
     <div className="App">
- 
+      
     </div>
   );
 }
