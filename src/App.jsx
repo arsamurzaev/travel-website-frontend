f6e13059ce0098da7b0a15b8400c933ea6d7ebd1
Martazanov <saidmartazanov1@gmail.com>
2022-12-22 18:51:13 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-  // import Tour from './components/Tour/Tour';
-  // import Header from './components/Header'
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
+import Tour from './components/Tour/Tour';
+import Header from './components/Header'
+import "bootstrap/dist/css/bootstrap.min.css";
 import MainPage from "./pages/MainPage";
 import { Route, Routes } from "react-router";
 import MyProfile from "./pages/MyProfile/index";
 
-
 const App = () => {
   return (
     <div className="App">
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <Header />
-      <Tour />
-
-=======
->>>>>>> 67c018ee1ca6a7b5302a64dfce3163e1a033a158
-=======
-      {/* <Header />
-      <Tour /> */}
       <Routes>
-        <Route path="/" element={<MainPage/>}/>
-        <Route path="/profile/*" element={<MyProfile/>}/>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/profile/*" element={<MyProfile />} />
       </Routes>
->>>>>>> main
     </div>
   );
 };
